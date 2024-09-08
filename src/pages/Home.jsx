c4af63bec816ca2cec3a631797a97acc89e95ba1
Lekhenar <131255968+Lekhenar@users.noreply.github.com>
2024-09-08 23:48:59 +0200

    Update Home.jsx
    modif iron.png

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import iron from "../assets/img/iron.png";
+import iron from "../assets/img/Iron.png";
 import marvelComic from "../assets/img/header-im.jpg";
 import spider from "../assets/img/spiderman.jpg";
 import spiderman from "../assets/img/spider.png";
